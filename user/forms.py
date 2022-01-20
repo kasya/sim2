eea630bcabb18f5f8b36604852b0a297f207f10b
Kate <kate@kgthreads.com>
2022-01-19 21:36:09 -0800

    Create signup and profile pages.
    Add signup form and form validation. Add empty profile page. Add logout link.

--- user/forms.py
+++ user/forms.py
@@ -1,14 +1,58 @@
 from django import forms
+from django.contrib.auth import authenticate, hashers
+from django.contrib.auth import login as auth_login
 from django.forms import ModelForm
+from django.urls import reverse
 
 from user.models import User
 
 
 class LoginForm(ModelForm):
 
+  remember = forms.BooleanField(label="Remember me", required=False)
+
+  def clean(self):
+    cleaned_data = super().clean()
+    if not authenticate(username=self.cleaned_data['email'],
+                        password=self.cleaned_data['password']):
+      raise forms.ValidationError('Invalid login or password.')
+
+    return cleaned_data
+
+  def login(self, request):
+    user = authenticate(username=self.cleaned_data['email'],
+                        password=self.cleaned_data['password'])
+    auth_login(request, user)
+
+    if self.cleaned_data['remember']:
+      request.session.set_expiry(86400 * 7)
+
+    return reverse('profile')
+
   class Meta:
     model = User
     fields = ['email', 'password']
+    widgets = {
+        'password': forms.PasswordInput(),
+    }
+
+
+class SignupForm(ModelForm):
+
+  def save(self, commit=True):
+    user = super().save(commit=False)
+
+    user.set_password(self.cleaned_data['password'])
+    user.username = self.cleaned_data['email']
+
+    if commit:
+      user.save()
+
+    return user
+
+  class Meta:
+    model = User
+    fields = ('email', 'first_name', 'last_name', 'password')
     widgets = {
         'password': forms.PasswordInput(),
     }
--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('login', views.Login.as_view(), name='login'),
     path('profile', views.Profile.as_view(), name='profile'),
-
-    # path('signup', views.signup, name='signup'),
+    path('signup', views.Signup.as_view(), name='signup'),
+    path('logout', views.Logout.as_view(), name='logout'),
 ]
