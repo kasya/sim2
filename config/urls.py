7f7bda58c8263625280cb1c8e029327f1ef1a125
Kate <kate@kgthreads.com>
2022-01-21 19:05:54 -0800

    Fix pull request issues.

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("apps.user.urls")),
+    path('', include('apps.user.urls')),
 ]
