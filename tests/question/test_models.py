badd7bb620e739ed08e7aa00ea9b8ce4f228ed8b
Kate <kate@kgthreads.com>
2022-02-07 17:33:00 -0800

    Add fixtures for question model testing. (#29)
    Add fixtures for question model testing.
    Divide tests into smaller test cases for each particular check.
    Put attempt, exam, user and question objects into setUp() for tests.

--- tests/question/test_models.py
+++ tests/question/test_models.py
@@ -9,39 +9,28 @@ from apps.question.models import Answer, Question, QuestionCategory
 class QuestionModelTestCase(TestCase):
   """Test cases for Question models."""
 
-  def setUp(self):
-    question_category = QuestionCategory.objects.create(name='Category 1')
-    subject = Subject.objects.create(name='Subject 1')
-    exam = Exam.objects.create(name='Exam 1', subject=subject)
-    correct_answers = Answer.objects.create(text='Answer 1')
-    wrong_answers = Answer.objects.create(text='Answer 2')
-    question = Question.objects.create(text='Question 1',
-                                       category=question_category,
-                                       exam=exam)
-    question.correct_answers.add(correct_answers)
-    question.wrong_answers.add(wrong_answers)
+  fixtures = [
+      'answer.json', 'exam.json', 'exam_attempt.json', 'question.json',
+      'question_category.json', 'subject.json', 'user.json'
+  ]
 
   def test_answer_model(self):
     """Unit test for answer model."""
 
-    answer = Answer.objects.get(text='Answer 1')
+    answer = Answer.objects.get(id=1)
 
-    self.assertEqual(str(answer), f'<Answer>: {answer.text}, id# {answer.id}')
+    self.assertEqual(str(answer), f'<Answer>: Correct!, id# 1')
 
   def test_question_model(self):
     """Unit test for question model."""
 
-    question = Question.objects.get(text="Question 1")
+    question = Question.objects.get(id=1)
 
-    self.assertEqual(str(question),
-                     f'<Question>: {question.text}, id# {question.id}:')
-    self.assertEqual(Question.objects.count(), 1)
+    self.assertEqual(str(question), f'<Question>: Question 1, id# 1:')
 
   def test_question_category(self):
     """Unit test for question category model."""
 
     category = QuestionCategory.objects.get(name="Category 1")
 
-    self.assertEqual(str(category),
-                     f'<Category>: {category.name}, id# {category.id}')
-    self.assertEqual(QuestionCategory.objects.count(), 1)
+    self.assertEqual(str(category), f'<Category>: Category 1, id# 1')
--- tests/question/test_views.py
+++ tests/question/test_views.py
@@ -12,174 +12,192 @@ from apps.question.serializers import QuestionSerializer
 from apps.user.models import User
 
 
-class UserViewTestCase(TestCase):
+class QuestionViewTestCase(TestCase):
   """Test cases for question views."""
 
   client = Client()
-  username = 'John'
-  password = 'password'
-  extra_user_username = 'Jane'
-  extra_user_password = 'multipass'
-  exam_name = 'Exam 1'
-  subject_name = 'Subject 1'
-  question_text = 'Question 1'
+  fixtures = [
+      'answer.json', 'exam.json', 'exam_attempt.json', 'question.json',
+      'question_category.json', 'subject.json', 'user.json'
+  ]
+  user_password = 'mypassword'
 
   def setUp(self):
 
-    question_category = QuestionCategory.objects.create(name='Category 1')
-    subject = Subject.objects.create(name=self.subject_name)
-    exam = Exam.objects.create(name=self.exam_name, subject=subject)
-    exam1 = Exam.objects.create(name='Exam 2', subject=subject)
+    self.attempt = ExamAttempt.objects.get(id=1)
+    self.exam = Exam.objects.get(id=1)
+    self.question = Question.objects.get(id=1)
+    self.user = User.objects.get(id=4)
 
-    correct_answers = Answer.objects.create(text='Answer 1')
-    wrong_answers = Answer.objects.create(text='Answer 2')
-    question = Question.objects.create(text=self.question_text,
-                                       category=question_category,
-                                       exam=exam)
-    self.question2 = Question.objects.create(text='Question 2',
-                                             category=question_category,
-                                             exam=exam1)
-    question.correct_answers.add(correct_answers)
-    question.wrong_answers.add(wrong_answers)
-    User.objects.create_user(username=self.username, password=self.password)
-    User.objects.create_user(username=self.extra_user_username,
-                             password=self.extra_user_password)
+  def test_api_get_question_anonymous(self):
+    """Try to access page with anonymous user."""
+
+    response = self.client.get(
+        reverse('question_api', kwargs={'attempt_id': self.attempt.id}))
+    self.assertEqual(response.status_code, 404)
+
+  def test_api_get_question_unauthorized_user(self):
+    """Try to login unauthorized user and access page."""
+
+    user = User.objects.get(id=5)
+    self.client.login(username=user.username, password=self.user_password)
+    response = self.client.get(
+        reverse('question_api', kwargs={'attempt_id': self.attempt.id}))
+    self.assertEqual(response.status_code, 404)
 
   def test_api_get_question(self):
-    """Check that method send the correct question to frontend."""
+    """Check that method sends correct data to frontend."""
 
-    exam = Exam.objects.get(name=self.exam_name)
-    user = User.objects.get(username=self.username)
-    extra_user = User.objects.get(username=self.extra_user_username)
-    attempt = ExamAttempt.objects.create(user=user, exam=exam)
-    question = Question.objects.get(text=self.question_text)
-    attempt.questions.add(question)
-
-    response = self.client.get(
-        reverse('question_api', kwargs={'attempt_id': attempt.id}))
-    self.assertEqual(response.status_code, 404)
-
-    # try to login different user and access page
-    self.client.login(username=extra_user.username,
-                      password=self.extra_user_password)
-    response = self.client.get(
-        reverse('question_api', kwargs={'attempt_id': attempt.id}))
-    self.assertEqual(response.status_code, 404)
-
-    self.client.login(username=user.username, password=self.password)
-
+    self.client.login(username=self.user.username, password=self.user_password)
     with mock.patch('random.shuffle', return_value=lambda x: x):
       response = self.client.get(
-          reverse('question_api', kwargs={'attempt_id': attempt.id})).render()
+          reverse('question_api',
+                  kwargs={'attempt_id': self.attempt.id})).render()
+
       data = json.loads(response.content)
       self.assertEqual(response.status_code, 200)
-      self.assertEqual(QuestionSerializer(question).data, data)
+      self.assertEqual(QuestionSerializer(self.question).data, data)
 
-  def test_api_post_question(self):
+  def test_api_get_question_no_more_questions(self):
     """
-    Check that method get correct data 
-    from frontend and save it to database.
+    Check that method returns status code 200 
+    when there's no more unanswered questions.
     """
 
-    exam = Exam.objects.get(name=self.exam_name)
-    user = User.objects.get(username=self.username)
-    attempt = ExamAttempt.objects.create(user=user, exam=exam)
-    question = Question.objects.get(text=self.question_text)
-    attempt.questions.add(question)
-    correct_answer = Answer.objects.get(text='Answer 1')
-    wrong_answer = Answer.objects.get(text='Answer 2')
+    self.client.login(username=self.user.username, password=self.user_password)
+    response = self.client.get(
+        reverse('question_api', kwargs={'attempt_id': self.attempt.id}))
 
-    self.client.login(username=user.username, password=self.password)
+    self.assertEqual(response.status_code, 200)
 
-    # try to send data in without answer ids
+  def test_api_post_question_anonymous(self):
+    """Try to access page with anonymous user."""
+
     response = self.client.post(reverse('question_api',
-                                        kwargs={'attempt_id': attempt.id}),
-                                data={
-                                    'question_id': 1
-                                }).render()
+                                        kwargs={'attempt_id': self.attempt.id}),
+                                data={'question_id': 1})
 
     self.assertEqual(response.status_code, 404)
+
+  def test_api_post_question_404(self):
+    """
+    Check that method raise status code 404
+    if required data is missing.
+    """
+
+    self.client.login(username=self.user.username, password=self.user_password)
+    invalid_data = ({}, {'question_id': 1})
+
+    for data in invalid_data:
+      response = self.client.post(reverse(
+          'question_api', kwargs={'attempt_id': self.attempt.id}),
+                                  data=data)
+      self.assertEqual(response.status_code, 404)
+
+  def test_api_post_question(self):
+    """Check that method creates Answer Attempt and saves answer to db."""
+
+    correct_answer = Answer.objects.get(id=1)
+    attempt = ExamAttempt.objects.create(user=self.user, exam=self.exam)
+
+    self.client.login(username=self.user.username, password=self.user_password)
 
     response = self.client.post(reverse('question_api',
                                         kwargs={'attempt_id': attempt.id}),
                                 data={
                                     'answers': [correct_answer.id],
-                                    'question_id': 1
-                                }).render()
+                                    'question_id': self.question.id
+                                })
 
     self.assertEqual(response.status_code, 201)
     answer_attempt = AnswerAttempt.objects.get(attempt=attempt.id)
-    self.assertIsNotNone(answer_attempt)
     self.assertEqual(answer_attempt.answers.first().id, correct_answer.id)
-    # Check that answer id in db changes when we change our answers.
 
-    expected_result = wrong_answer
+  def test_api_post_update_answers(self):
+    """Check that answer id in db changes when we change our answers."""
+
+    attempt = ExamAttempt.objects.create(user=self.user, exam=self.exam)
+    correct_answer = Answer.objects.get(id=1)
+    wrong_answer = Answer.objects.get(id=2)
+
+    self.client.login(username=self.user.username, password=self.user_password)
+
+    response = self.client.post(reverse('question_api',
+                                        kwargs={'attempt_id': attempt.id}),
+                                data={
+                                    'answers': [correct_answer.id],
+                                    'question_id': self.question.id
+                                })
 
     response = self.client.post(reverse('question_api',
                                         kwargs={'attempt_id': attempt.id}),
                                 data={
                                     'answers': [wrong_answer.id],
-                                    'question_id': 1
-                                }).render()
+                                    'question_id': self.question.id
+                                })
     answer_attempt = AnswerAttempt.objects.get(attempt=attempt.id)
     self.assertIsNotNone(answer_attempt)
-    self.assertEqual(answer_attempt.answers.first().id, expected_result.id)
+    self.assertEqual(answer_attempt.answers.first().id, wrong_answer.id)
 
-  def test_get_attempt_question_answers(self):
-    """Check that method return correct question and answers to it."""
+  def test_get_attempt_question_answers_anonymous(self):
+    """Check that method raise status code 404 for anonymous user."""
 
-    exam = Exam.objects.get(name=self.exam_name)
-    subject = Subject.objects.get(name=self.subject_name)
-    user = User.objects.get(username=self.username)
-    attempt = ExamAttempt.objects.create(user=user, exam=exam)
-    question = Question.objects.get(text=self.question_text)
-    answer_attempt = AnswerAttempt.objects.create(attempt=attempt,
-                                                  question=question)
-    correct_answer = Answer.objects.get(text='Answer 1')
-
-    # Check for anonymous user.
     response = self.client.get(
         reverse('question_answers_api',
                 kwargs={
-                    'attempt_id': attempt.id,
+                    'attempt_id': self.attempt.id,
+                    'question_id': self.question.id
+                }))
+    self.assertEqual(response.status_code, 404)
+
+  def test_get_attempt_wrong_question(self):
+    """Check for case when question not in this attempt."""
+
+    question = Question.objects.get(id=2)
+    self.client.login(username=self.user.username, password=self.user_password)
+
+    response = self.client.get(
+        reverse('question_answers_api',
+                kwargs={
+                    'attempt_id': self.attempt.id,
                     'question_id': question.id
                 }))
+
     self.assertEqual(response.status_code, 404)
 
-    # Check for case when question not in this attempt.
-    self.client.login(username=user.username, password=self.password)
+  def test_get_attempt_no_question(self):
+    """Check for case when question does not exist in db."""
+
+    self.client.login(username=self.user.username, password=self.user_password)
 
     response = self.client.get(
         reverse('question_answers_api',
                 kwargs={
-                    'attempt_id': attempt.id,
-                    'question_id': self.question2.id
+                    'attempt_id': self.attempt.id,
+                    'question_id': 10,
                 }))
 
     self.assertEqual(response.status_code, 404)
 
-    # Check for case when question does not exist in db.
-    response = self.client.get(
-        reverse('question_answers_api',
-                kwargs={
-                    'attempt_id': attempt.id,
-                    'question_id': 5
-                }))
+  def test_get_api_attempt_question_answers(self):
+    """Check that method send correct data to frontend."""
 
-    self.assertEqual(response.status_code, 404)
-
-    attempt.questions.add(question)
+    correct_answer = Answer.objects.get(id=1)
+    answer_attempt = AnswerAttempt.objects.create(attempt=self.attempt,
+                                                  question=self.question)
     answer_attempt.answers.add(correct_answer)
+
+    self.client.login(username=self.user.username, password=self.user_password)
     with mock.patch('random.shuffle', return_value=lambda x: x):
 
       response = self.client.get(
           reverse('question_answers_api',
                   kwargs={
-                      'attempt_id': attempt.id,
-                      'question_id': question.id
+                      'attempt_id': self.attempt.id,
+                      'question_id': self.question.id
                   }))
 
       self.assertEqual(response.status_code, 200)
       self.assertEqual(response.data['answer_ids'], [correct_answer.id])
       self.assertEqual(response.data['question'],
-                       QuestionSerializer(question).data)
+                       QuestionSerializer(self.question).data)
