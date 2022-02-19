232e2274e213f2ba16790aaf2a2b62669ec320b2
Kate <kate@kgthreads.com>
2022-02-18 16:38:24 -0800

    Resolve pull request issues.

---
+++ apps/core/templatetags/sub.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='sub')
+def sub_filter(value, arg):
+  """Subtract arg from value."""
+
+  return int(value) - int(arg)
