2b8c1d3597f5bbaf980724546c6536ddca809882
Kate <kate@kgthreads.com>
2022-10-02 17:09:16 -0700

    Setup localization and translation.

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+from django.utils.translation import gettext_lazy as _
+
 # pylint: disable=wildcard-import
 from config.secrets import *
 
@@ -62,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.locale.LocaleMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -159,13 +162,22 @@ SESSION_COOKIE_AGE = 86400
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('ru', _('Russian')),
+)
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+DATETIME_FORMAT = 'N j, P'
+
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'apps/static')]
 
